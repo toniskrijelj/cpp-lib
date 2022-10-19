2d9f587c09802bee68c4f3d1e41574ff116f5e38
NoHaxJustSoFlo <58442140+toniskrijelj@users.noreply.github.com>
2022-10-20 00:10:20 +0200

    Update tree.cpp

--- tree/main.cpp
+++ tree/main.cpp
@@ -21,41 +21,54 @@ vector<int> adj[mxN];
 int up[mxlogN][mxN], down[mxN], in[mxN], out[mxN], timer_;
 int a[mxN];
 
-void dfs(int i, int p=1, int d=0)
+void dfs(int i=1, int p=1, int d=0)
 {
-    up[0][i]=p, down[i]=d, in[i] = timer_++;
-    for(int j = 1; j < mxlogN; j++) up[j][i]=up[j-1][up[j-1][i]];
-    for(int j : adj[i]) if(j!=p) dfs(j, i, d+1);
-    out[i] = timer_;
+    up[0][i]=p, down[i]=d, in[i]=timer_++;
+    for(int j=1; j<mxlogN; j++) up[j][i]=up[j-1][up[j-1][i]];
+    for(int j:adj[i]) if(j!=p) dfs(j,i,d+1);
+    out[i]=timer_;
 }
 int goup(int i, int k)
 {
-    if(!k) return i;
-    for(int j = mxlogN-1; j>=0; j--)
-    {
-        if(k>(1<<j))
-        {
-            k-=1<<j;
-            i = up[j][i];
-        }
-    }
-    return up[0][i];
+    for(int j=mxlogN-1; j>=0; j--) if(k&1<<j) i=up[j][i];
+    return i;
 }
 int lca(int i, int j)
 {
-    if(down[i] < down[j]) swap(i,j);
-    i = goup(i, down[i]-down[j]);
+    if(down[i]<down[j]) swap(i,j);
+    i=goup(i, down[i]-down[j]);
     if(i==j) return i;
-    for(int k = mxlogN-1; k>=0; k--)
-    {
-        if(up[k][i]!=up[k][j])
-        {
-            i = up[k][i], j = up[k][j];
-        }
-    }
+    for(int k=mxlogN-1; k>=0; k--) if(up[k][i]!=up[k][j]) i=up[k][i], j=up[k][j];
     return up[0][i];
 }
 int dist(int i, int j)
 {
     return down[i]+down[j]-2*down[lca(i,j)];
+}
+///------------------ seg tree
+int sz;
+void build(int n)
+{
+    sz=1;
+    while(sz<n) sz<<=1;
+}
+ll sum[mxN];
+void add(int l, int r, ll v, int i=0, int Lx=0, int Rx=sz)
+{
+    if(Rx<=l || Lx>=r) return;
+    if(Lx>=l && Rx<=r)
+    {
+        sum[i]+=v;
+        return;
+    }
+    int m=(Rx+Lx)/2;
+    add(l,r,v,i*2+1,Lx,m);
+    add(l,r,v,i*2+2,m,Rx);
+}
+ll get(int pos, int i=0, int Lx=0, int Rx=sz)
+{
+    if(Rx-Lx==1) return sum[i];
+    int m=(Rx+Lx)/2;
+    if(pos<m) return sum[i]+get(pos,i*2+1,Lx,m);
+    else return sum[i]+get(pos,i*2+2,m,Rx);
 }
